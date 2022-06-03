2edd00077f7a6c529fbd643b663d236b58e0bea7
alperalin <alperalin@gmail.com>
2022-06-03 12:49:51 +0300

    - Theme support added.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,28 +10,55 @@ import './App.scss';
 // Element
 function App() {
   // States
+  const prefersLightMQ = '(prefers-color-scheme: light)';
+
   const [theme, setTheme] = useState(
-    window.localStorage.getItem('theme') || 'light'
+    window.matchMedia(prefersLightMQ).matches
+      ? 'light'
+      : 'dark' || window.localStorage.getItem('theme') || 'light'
   );
 
   const [currentArtist, setCurrentArtist] = useState(
     () => JSON.parse(window.localStorage.getItem('currentArtist')) || ''
   );
 
+  // Use Effect
+  // Keeps track of OS theme changes
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(prefersLightMQ);
+    const handleChange = () => {
+      setTheme(mediaQuery.matches ? 'light' : 'dark');
+    };
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
+  // When theme state changes
+  // It sets theme value to localStorage
+  // And changes body element's class
+  useEffect(() => {
+    window.localStorage.setItem('theme', theme);
+
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [theme]);
+
+  // Sets clicked artist to localStorage
   useEffect(() => {
     window.localStorage.setItem('currentArtist', JSON.stringify(currentArtist));
   }, [currentArtist]);
 
-  useEffect(() => {
-    window.localStorage.setItem('theme', theme);
-  }, [theme]);
-
+  // Functions
   function handleThemeChange() {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
   }
 
+  // APP
   return (
-    <section className={`app${theme === 'dark' && ' app--dark'}`}>
+    <section className="app">
       <header className="app__header">
         <Image size="small" src="./logo192.png" alt="React Logo" />
         <button type="button" onClick={handleThemeChange}>
